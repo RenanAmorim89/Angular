8faed37e1b8a6e5d1873f0ee58a3b830543a8eb9
Renan Amorim <renanamorim89@hotmail.com>
2023-06-14 10:21:15 -0300

    Marmita-componente-das-marmitas-2

--- Fernando/marmitaria/src/app/packages/package-list/package-list.component.ts
+++ Fernando/marmitaria/src/app/packages/package-list/package-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Package } from './package.model';
 
 @Component({
   selector: 'app-package-list',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackageListComponent implements OnInit {
 
-  packages = [];
+  packages: Package[  ] = [
+  new Package('Churrasco', 'Churrasco de Picanha','https://minervafoods.com/wp-content/uploads/2023/02/Acompanhamento-para-churrasco-confira-8-opcoes-saborosas-scaled.jpg'),
+  ];
 
   constructor(){}
 
