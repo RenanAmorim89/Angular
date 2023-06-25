e42ae6664fb40583cd34832ffb7d56e696ece643
Renan Amorim <117317062+RenanAmorim89@users.noreply.github.com>
2023-06-25 10:07:48 -0300

    Event binding

--- Loiane/data-binding/src/app/data-binding/data-binding.component.ts
+++ Loiane/data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,22 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
 })
 export class DataBindingComponent {
-
-  url: string = 'http://loiane.training'
+  url: string = 'http://loiane.training';
 
   cursoAngular: boolean = true;
 
-  urlImagem = 'http://lorempixel.com.br/500/400/?1'
+  urlImagem = 'http://lorempixel.com.br/500/400/?1';
 
-  getValor(){
-    return 1
-  }
-  
-  getCurtirCurso(){
-    return true
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
+  getValor() {
+    return 1;
   }
 
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    // console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+    
+  }
+
+  salvarValor(valor: string) {
+    this.valorSalvo = valor
+  }
+  //* se o mouse estiver sobre o botão, o valor de isMouseOver será true, se não, será false
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver
+
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
 }
